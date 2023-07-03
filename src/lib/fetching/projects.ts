f7e07f1e6142f61349005c1a38e546561c40d84e
Jepri <jepri.creations@gmail.com>
2023-07-03 17:24:21 +0700

    💎 style: animation on dropdown exit

--- src/lib/fetching/projects.ts
+++ src/lib/fetching/projects.ts
@@ -21,3 +21,22 @@ export const fetchProjects = async () => {
 
   return projects?.filter((project) => project.roles.length > 0)
 }
+
+export const fetchProject = async (id: string) => {
+  const supabase = createServerClient()
+
+  const { data, error } = await supabase
+    .from('projects')
+    .select(
+      `id, updated_at, name, summary, categories, icon_url, 
+      roles(name, exp_level, rewards, work_mode, status)`
+    )
+    .eq('id', id)
+    .single()
+
+  if (error) {
+    throw new Error(error.message)
+  }
+
+  return data
+}
