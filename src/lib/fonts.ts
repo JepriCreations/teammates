657d4e259e1807fe3a9d071261b6677fdd233395
Jepri <jepri.creations@gmail.com>
2024-03-14 12:31:17 +0700

    refactor: set variable font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,14 +1,8 @@
-import { Noto_Color_Emoji as Emoji, Outfit as FontSans } from 'next/font/google'
+import { Outfit as FontSans } from 'next/font/google'
 
 export const fontSans = FontSans({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-sans',
-  weight: ['300', '400', '500', '600', '700'],
-})
-
-export const fontEmoji = Emoji({
-  subsets: ['emoji'],
-  weight: '400',
-  variable: '--emoji',
+  weight: 'variable',
 })
