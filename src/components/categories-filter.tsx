ff4d3519766a93d4560aa58edb211742b6eb2d9f
Jepri <jepri.creations@gmail.com>
2024-03-25 12:07:26 +0700

    feature: Added scroll buttons on categories filter

--- src/components/categories-filter.tsx
+++ src/components/categories-filter.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { useRef } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { CATEGORIES, CATEGORIES_ICONS } from '@/constants/projects'
 import { PARAMS_KEYS } from '@/constants/routes'
 
 import { useSetSearchParams } from '@/hooks/use-set-search-params'
+import { Icon } from '@/components/ui/icon'
+import { IconButton } from '@/components/ui/icon-button'
 import { useDictionary } from '@/components/providers/dictionary-provider'
 
 export const CategoriesFilter = () => {
@@ -12,6 +15,7 @@ export const CategoriesFilter = () => {
   const setSearchParams = useSetSearchParams()
   const searchParams = useSearchParams()
   const categoryParam = searchParams.get(PARAMS_KEYS.CATEGORY)
+  const ref = useRef<HTMLDivElement>(null)
 
   const categoriesItems = CATEGORIES(t).map((category) => ({
     ...category,
@@ -22,10 +26,64 @@ export const CategoriesFilter = () => {
     setSearchParams({ name: PARAMS_KEYS.CATEGORY, value })
   }
 
+  const scroll = (scrollOffset: number, duration: number = 400) => {
+    if (ref.current) {
+      const currentElement = ref.current
+      const startTime = performance.now()
+      const startScrollLeft = currentElement.scrollLeft
+      const endTime = startTime + duration
+      const easeInOutQuad = (t: number) =>
+        t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+
+      const animateScroll = () => {
+        const currentTime = performance.now()
+        const elapsedTime = Math.min(1, (currentTime - startTime) / duration)
+        currentElement.scrollLeft =
+          startScrollLeft + scrollOffset * easeInOutQuad(elapsedTime)
+
+        if (currentTime < endTime) {
+          requestAnimationFrame(animateScroll)
+        }
+      }
+
+      animateScroll()
+    }
+  }
+
+  const handleScroll = () => {
+    if (ref.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = ref.current
+      const isAtBeginning = scrollLeft === 0
+      const isAtEnd = scrollLeft + clientWidth === scrollWidth
+      const leftButton = document.getElementById('leftButton')
+      const rightButton = document.getElementById('rightButton')
+      if (leftButton)
+        leftButton.style.visibility = isAtBeginning ? 'hidden' : 'visible'
+      if (rightButton)
+        rightButton.style.visibility = isAtEnd ? 'hidden' : 'visible'
+    }
+  }
+
   return (
     <div className="relative w-full overflow-hidden">
-      <span className="absolute inset-y-0 left-0 w-8 bg-gradient-to-r from-background to-transparent to-[16px]" />
-      <div className="scrollbar-hidden flex w-full flex-nowrap items-center gap-3 overflow-x-auto px-3">
+      <span
+        id="leftButton"
+        className="absolute inset-y-0 left-0 z-20 hidden w-20 bg-gradient-to-r from-background from-60% to-transparent to-90% md:block"
+        style={{ visibility: 'hidden' }}
+      >
+        <IconButton
+          variant="standard"
+          onClick={() => scroll(-1 * (ref.current?.clientWidth ?? 124) - 48)}
+          className="absolute left-0 top-1/2 z-20 translate-y-[-50%]"
+        >
+          <Icon symbol="chevron_left" />
+        </IconButton>
+      </span>
+      <div
+        ref={ref}
+        onScroll={handleScroll}
+        className="scrollbar-hidden flex w-full flex-nowrap items-center gap-3 overflow-x-auto px-3"
+      >
         {categoriesItems.map(({ value, label, icon }) => (
           <CategoryItem
             key={value}
@@ -36,7 +94,19 @@ export const CategoriesFilter = () => {
           />
         ))}
       </div>
-      <span className="absolute inset-y-0 right-0 w-8 bg-gradient-to-l from-background to-transparent to-[16px]" />
+      <span
+        id="rightButton"
+        className="absolute inset-y-0 right-0 z-20 hidden w-20 bg-gradient-to-l from-background from-60% to-transparent to-90% md:block"
+        style={{ visibility: 'visible' }}
+      >
+        <IconButton
+          variant="standard"
+          onClick={() => scroll((ref.current?.clientWidth ?? 124) - 48)}
+          className="absolute right-0 top-1/2 z-20 translate-y-[-50%]"
+        >
+          <Icon symbol="chevron_right" />
+        </IconButton>
+      </span>
     </div>
   )
 }
